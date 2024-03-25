7871714b3c653f93bc7c272fa3c5b53d23a051cd
Ed Calderon <edwardcalderon2010@gmail.com>
2024-03-25 20:50:27 +1000

    EC-001: initial commit for spring shell starter project

---
+++ src/main/java/com/init/spring/shell/springshellstarter/SpringShellStarterApplication.java
@@ -0,0 +1,13 @@
+package com.init.spring.shell.springshellstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/init/spring/shell/springshellstarter/SpringShellStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.spring.shell.springshellstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
